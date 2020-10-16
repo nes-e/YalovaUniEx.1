2d0338f8f612ff0292e5b09352ebed7dcb5715b8
nes-e <ylmazenes969@gmail.com>
2020-10-16 08:08:47 +0300

    HelloWorld!InAndroidStudio-YalovaUni

---
+++ app/src/main/java/com/example/ders1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ders1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("Ders1", "Hello world ! Yalova UNI!");
+
+
+
+
+    }
+}
